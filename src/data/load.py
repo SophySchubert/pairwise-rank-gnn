bd26accabe7aee0183e4051df779bf67e90b8cab
Sophia Schubert <sophia.schubert@adesso.de>
2024-12-13 15:11:38 +0100

    Attempt to change input to tensor

--- src/data/load.py
+++ src/data/load.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import tensorflow as tf
 from spektral.datasets import TUDataset, QM9
 from data.ogb_helper import OGBDataset, ogb_available_datasets
 
@@ -10,7 +10,7 @@ def _load_data(name: str):
     '''
 
     if name == 'QM9':
-        dataset = QM9(amount=100000)# 1000 and 100000 ok
+        dataset = QM9(amount=1000)# 1000 and 100000 ok
     elif name in TUDataset.available_datasets():
         dataset = TUDataset(name)
     elif name in ogb_available_datasets():
@@ -19,17 +19,6 @@ def _load_data(name: str):
         raise ValueError(f'Dataset {name} unknown')
 
     return dataset, dataset.n_labels
-
-def _sample_pairs(dataset):
-    '''
-    Sample pairs of graphs from a dataset
-    '''
-    assert(len(dataset) % 2 == 0)
-
-    _pair_a = dataset[::2]
-    _pair_b = dataset[1::2]
-
-    return list(zip(_pair_a, _pair_b))
 
 def _split_data(data, train_test_split, seed):
     '''
@@ -41,8 +30,25 @@ def _split_data(data, train_test_split, seed):
     split = int(train_test_split * len(data))
     idx_train, idx_test = np.split(idxs, [split])
     train, test = data[idx_train], data[idx_test]
+    return train, test
 
-data    return {'data':train, 'idx':[1,2,3]}, {'data':test, 'idx':[1,2,3]}
+def _sample_pairs(dataset):
+    '''
+    Sample pairs of graphs from a dataset
+    '''
+    assert(len(dataset) % 2 == 0)
+    len_dataset = len(dataset)
+    dataset = [d.y for d in dataset]
+    _pairs_even = dataset[::2]
+    _pairs_odd = dataset[1::2]
+    _pairs_even_ids = range(0, len_dataset, 2)
+    _pairs_odd_ids = range(1, len_dataset, 2)
+
+    data = list(zip(_pairs_even, _pairs_odd))
+    ids = list(zip(_pairs_even_ids, _pairs_odd_ids))
+    targets = [max(d) for d in data]
+
+    return ids, targets
 
 def get_data(config):
     seed = config['seed']
@@ -57,84 +63,7 @@ def get_data(config):
     train_data, test_data = _split_data(data, train_test_split, seed)
     # Create pairs
     if pairwise:
-        train_data = _sample_pairs(train_data)
-        test_data = _sample_pairs(test_data)
+        train_pairs, train_targets = _sample_pairs(train_data)
+        test_pairs, test_targets = _sample_pairs(test_data)
 
-    return train_data, test_data
-
-
-
-
-
-if __name__ == "__main__":
-    import tensorflow as tf
-    from spektral.data import Graph
-
-    # Example graph object
-    graph = Graph(x=[[1, 2], [3, 4]], a=[[0, 1], [1, 0]], e=[[0.5], [0.5]])
-
-    # Convert node features to tensor
-    x_tensor = tf.convert_to_tensor(graph.x, dtype=tf.float32)
-
-    # Convert adjacency matrix to tensor
-    a_tensor = tf.convert_to_tensor(graph.a, dtype=tf.float32)
-
-    # Convert edge features to tensor (if available)
-    e_tensor = tf.convert_to_tensor(graph.e, dtype=tf.float32) if graph.e is not None else None
-
-    print("Node features tensor:", x_tensor)
-    print("Adjacency matrix tensor:", a_tensor)
-    print("Edge features tensor:", e_tensor)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return train_data, train_pairs, train_targets, test_data, test_pairs, test_targets
--- src/experiment.py
+++ src/experiment.py
@@ -27,9 +27,13 @@ if __name__ == '__main__':
     ######################################################################
 
     # Load data and split it in train and test sets
-    train_data, test_data = get_data(config)
-    train_y = [(tuple[0].y, tuple[1].y) for tuple in train_data]
-    test_y = [(tuple[0].y, tuple[1].y) for tuple in test_data]
+    train_graphs, train_pairs, train_y, test_graphs, test_pairs, test_y = get_data(config)
+    print(type(train_graphs))
+    print(train_graphs[0])
+    print(type(train_pairs))
+    print(train_pairs[0])
+    print(type(train_y))
+    print(train_y[0])
 
     # Initialize the model, optimizer, and loss function
     model = setup_model(config)
@@ -40,9 +44,15 @@ if __name__ == '__main__':
     ################################################################################
     # Fit model
     ################################################################################
-    # loader_train = BatchLoader(train_data, batch_size=config['batch_size'], mask=True)
-    # h = model.fit(loader_train.load(), steps_per_epoch=loader_train.steps_per_epoch, epochs=config['epochs'], verbose=2)
-    h = model.fit(train_data, batch_size=config['batch_size'], epochs=config['epochs'], verbose=2)
+#     loader_train = BatchLoader((train_graphs, train_pairs, train_y), batch_size=config['batch_size'], mask=True)
+#     h = model.fit(loader_train.load(), steps_per_epoch=loader_train.steps_per_epoch, epochs=config['epochs'], verbose=2)
+
+    train_graphs_x = np.array([graph.x for graph in train_graphs])
+    train_graphs_a = np.array([graph.a for graph in train_graphs])
+    train_graphs_e = np.array([graph.e for graph in train_graphs])
+    train_data = list(zip(train_graphs_x, train_graphs_a, train_graphs_e))
+    train = tf.data.Dataset.from_tensors((train_data, train_pairs, train_y))
+    h = model.fit(train, batch_size=config['batch_size'], epochs=config['epochs'], verbose=2)
     save_history(h, config['folder_path'])
     logger.info(f"Training done!")
     ################################################################################
