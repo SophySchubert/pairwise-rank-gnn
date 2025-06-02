80c58f5bee0b2366be057fc38decb54de3fe2dce
Sophia Schubert <sophia.schubert@adesso.de>
2025-06-02 22:00:21 +0200

    removed unused files

--- convertion.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-conda create --name convertion python=3.10
-conda activate convertion
-conda install --yes --file req_con.txt
-
--- experiment.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-conda create --name experiment python=3.6
-conda activate experiment
-conda install -c conda-forge cudatoolkit=11.2 cudnn=8.1.0
-conda install --yes --file req_exp.txt
-python -c "import tensorflow as tf; print(tf.__version__); print(tf.config.list_physical_devices('GPU'))"
-
